e9364731a39bb41f7acfdcf02b0d26bd729b39c7
Dawei Feng <dawei@daweis-iMac.local>
2017-05-28 14:14:32 -0700

    First commit

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+const DriverController = require('../controllers/drivers_controller');
+module.exports = (app) => {
+    // Whenever there is a incoming GET Method to route '/api', 
+    // run the callback function
+    app.get('/api', DriverController.greeting);
+
+    // we do not put () after the create because we do not want it to be run at the app booting up
+    app.post('/api/drivers',DriverController.create);
+}
